cca635c67780c1a219a1ce863766689a7d21b1c6
Anthony LASSERRE <lasserre@intechinfo.fr>
2018-05-28 15:49:02 +0200

    Commit fais par Anthony LASSERRE
    Sujet : Retour version
    Date :

--- vue/main_page.php
+++ vue/main_page.php
@@ -1,14 +1,30 @@
-<?php include('../modele/main_page_script_1.php'); ?>
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Main page</title>
-		<meta charset = ='UTF-8'>
-	</head>
-	<body>
-	<a href ='main_page.php'><button>
-		</form>
-		<center>
+  <head>
+    <title>PAGE PRINCIPALE</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <form action="../CONTROLEUR/search.php" method="get">
+      <select name="categorie">
+        <option value ="">Tout</option>
+        <option value="Informatique">Informatique</option>
+        <option value="High-tech">High-tech</option>
+      </select>
+      <select name="sous-categorie">
+        <option value ="">Tout</option>
+        <option value="Souris">Souris</option>
+        <option value="Clavier">Clavier</option>
+        <option value ="divers">Divers</option>
+        <option value ="ordinateur portable">Ordi</option> 
+        <option value ="Hardware"></option> 
+      </select> 
+      <button>Chercher</button><br> 
+ 
+    </form> 
+    <table border=1 cellspacing=2 style="text-align:center">
+      <?php include('../CONTROLEUR/Script_main_page_1.php'); ?>
+    </table>
 
-		</center>
-	</body>
+  </body>
 </html>
