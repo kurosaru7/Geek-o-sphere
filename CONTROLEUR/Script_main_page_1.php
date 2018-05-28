0c747479895447a015ea47bee0bc61ee0a803548
Anthony LASSERRE <lasserre@intechinfo.fr>
2018-05-28 12:18:08 +0200

    Commit fais par Anthony LASSERRE
    Sujet : Rajout de la fonction de recherche et modifs à faire sur le script de la BDD
    Date : 28/05/2018

--- CONTROLEUR/Script_main_page_1.php
+++ CONTROLEUR/Script_main_page_1.php
@@ -4,8 +4,25 @@
 	session_start();
 
 	$req="";
+	$a=0;
 	if (isset($_SESSION['articles']) && $_SESSION['articles'] != '') {
-		$req = 'WHERE categorie="'.$_SESSION['articles'].'"';
+		$req = ' categorie="'.$_SESSION['articles'].'"';
+		$a++;
+	}
+	$req2="";
+	$b=0;
+	if (isset($_SESSION['categorie']) && $_SESSION['categorie'] != '') {
+		$req2 = ' sous_categorie="'.$_SESSION['categorie'].'"';
+		$b++;
+	}
+
+	$reqf="";
+	if ($a == 1 && $b == 1) {
+		$reqf = 'WHERE ('.$req.' AND '.$req2.')';
+	} else if ($a == 1 && $b == 0) {
+		$reqf = 'WHERE '.$req;
+	} else if ($a == 0 && $b == 1) {
+		$reqf = 'WHERE '.$req2;
 	}
 
 	print ('<tr>
@@ -19,7 +36,7 @@
 			<td> Sous-Categorie 
 	');
 
-	$requete = $bdd->query('SELECT * FROM articles '.$req);
+	$requete = $bdd->query('SELECT * FROM articles '.$reqf);
 	while ( $donnees = $requete -> fetch()) {
 
 		print ('<tr>
@@ -30,7 +47,7 @@
 				<td>'.utf8_encode($donnees['quantite']).'
 				<td>'.utf8_encode($donnees['idMagasins']).'
 				<td>'.utf8_encode($donnees['prix']).'€
-				<td>'.utf8_encode($donnees['sous-categorie'])
+				<td>'.utf8_encode($donnees['sous_categorie'])
 		);
 
 	}
--- CONTROLEUR/search.php
+++ CONTROLEUR/search.php
@@ -2,6 +2,7 @@
 
 	session_start();
 	$_SESSION['articles'] = $_GET['categorie'];
+	$_SESSION['categorie'] = $_GET['sous-categorie'];
 
 	header('location: ../VUE/main_page.php')
 
