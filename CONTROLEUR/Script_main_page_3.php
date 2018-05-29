32c613fab8b4ea925884d1847b33131492be44a0
Unknown <otarinolive@gmail.com>
2018-05-29 15:30:47 +0200

    Auto stash before merge of "master" and "origin/master"
    Plus de $i=0

--- CONTROLEUR/Script_main_page_3.php
+++ CONTROLEUR/Script_main_page_3.php
@@ -2,7 +2,6 @@
 
 	$bdd = new PDO('mysql:host=localhost;dbname=geek-o-sphere', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-	$i = 0;
 	$requete = $bdd->query('SELECT DISTINCT sous_categorie FROM articles');
 	while ( $donnees = $requete -> fetch()) {
 
