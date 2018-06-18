ef5ad643aeabf12d1fd40373cc7c3f173897ff16
Anthony LASSERRE <lasserre@intechinfo.fr>
2018-06-18 18:40:53 +0200

    Commit fait par Anthony LASSERRE
    Sujet : Mise en place pour Chris
    Date : 18/06/2018

---
+++ VUE/historique.php
@@ -0,0 +1,24 @@
+<?php require("../MODEL/model.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="../VUE/style.css" type="text/css" media="all" />
+	<meta charset ="UTF-8">
+</head>
+<body><br>
+		<center>
+			<img src="../VUE/logo.png" class ="image" />
+			<ul>
+			   <li><a href="./Script_main_page.php">Accueil</a></li>
+			   <li><a href="./Script_client_page.php"><?php echo $_SESSION['pseudo'] ?></a></li>
+			   <li><a href="#">Panier</a></li>
+			   <li><a href="#"><?php $data=getOneAccount($_SESSION['pseudo']); $credit=$data->fetch(); echo $credit['credit'].' €'; ?></a></li>
+			   <li style="float:right"><a class="active" href="../CONTROLEUR/Script_main_page_disconnection.php">Déconnexion</a></li>
+			</ul>
+
+
+			
+		</center>
+	</body>
+</html>
