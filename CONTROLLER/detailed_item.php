c7a72d0d66c9db53c16d599a2a26e146278378bb
Unknown <otarinolive@gmail.com>
2018-06-20 14:46:59 +0200

    page items

---
+++ CONTROLLER/detailed_item.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if(isset($_SESSION['pseudo']) && isset($_SESSION['pwd'])) {
+  require("../MODEL/model.php");
+  $query = 'WHERE `idArticles` ='.$_GET['id'];
+  $item = getArticlesCustom($query);
+
+  $data = $item -> fetch();
+
+  $categorie = utf8_encode($data['categorie']);
+  $nom = utf8_encode($data['nom']);
+  $description = utf8_encode($data['description']);
+  $image =  utf8_encode($data['image']);
+  $quantite = utf8_encode($data['quantite']);
+  $prix =  utf8_encode($data['prix']);
+  $sous_categorie = utf8_encode($data['sous_categorie']);
+
+  include("../VIEW/detailed_item.php");
+}else{
+  include("../VIEW/authentification_requise.php");
+}
