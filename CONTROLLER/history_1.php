7b00b72985a44ce879fbc9386135d1697aeb2a5b
Anthony LASSERRE <lasserre@intechinfo.fr>
2018-06-20 14:47:29 +0200

    Commit fait par Anthony LASSERRE
    Sujet :
    Date :

--- CONTROLLER/history_1.php
+++ CONTROLLER/history_1.php
@@ -1,5 +1,30 @@
 <?php
 
+	echo "<table>
+			<th>Jour
+			<th>Heure
+			<th colspan=2>Article
+		 "
+	;
+
+	$test = false;
+
 	$query = historyCall();
+	while ( $data = $query -> fetch()) {
+
+		echo"<tr>
+			 <td>".$data['achats.date'].
+			"<td>".$data['achats.time'].
+			"<td>".utf8_encode($data['articles.nom']).
+			"<td><a href='detailed_item.php?id=".$data['achats.idArticles']."'><button>...</button></a>"
+		;
+		$test = true;
+
+	} if (!$test) {
+
+		echo"<tr><td><td><td colspan=2>Aucun achat effectuer";
+	}
+
+	print_r($data);
 
 ?>
